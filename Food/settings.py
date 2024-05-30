7df0f92f27c01d00e0b9127e25bba8719d6337fc
k7kaushal <82771451+k7kaushal@users.noreply.github.com>
2024-05-30 19:49:42 +0530

    settings update

--- Food/settings.py
+++ Food/settings.py
@@ -118,7 +118,7 @@ import os
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "staticfiles")
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
