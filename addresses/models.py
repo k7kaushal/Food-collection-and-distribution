6bae92cba3dde2da95188afc53f7be35521d923e
k7kaushal <82771451+k7kaushal@users.noreply.github.com>
2024-05-30 19:16:43 +0530

    deploy

---
+++ addresses/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+import geocoder
+# from phonenumber_field.modelfields import PhoneNumberField
+
+access_token = 'pk.eyJ1IjoiazdrYXVzaGFsIiwiYSI6ImNsaDRqZXh4NDBsc3IzaG81NnIwdHpvNGcifQ.Zkv2dg9ykTIQQlkyeyg_HQ'
+
+class Address(models.Model):
+    address = models.TextField()
+    lat = models.FloatField(blank = True, null = True)
+    long = models.FloatField(blank = True, null = True)
+
+    def save(self, *args, **kwargs):
+        g = geocoder.mapbox(self.address, key = access_token)
+        g = g.latlng
+        self.lat = g[0]
+        self.long = g[1]
+        return super(Address, self).save(*args, **kwargs)
+    
+class NGO(models.Model):
+    ngo_name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    ngo_type = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    # Contact = models.PhoneNumberField(null=False, blank=False, unique=True)
+    Contact = models.CharField(max_length=10, default = "12345678")
+    website = models.CharField(max_length = 100, default="https://www.akshayapatra.org/")
+    social = models.CharField(max_length=100, default="https://www.instagram.com/")
+    ngolat = models.FloatField(blank = True, null = True)
+    ngolong = models.FloatField(blank = True, null = True)
+
+    def save(self, *args, **kwargs):
+        g = geocoder.mapbox(self.location, key = access_token)
+        g = g.latlng
+        self.ngolat = g[0]
+        self.ngolong = g[1]
+        return super(NGO, self).save(*args, **kwargs)
+    
+class Event(models.Model):
+    event_name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    event_attendants = models.CharField(max_length=10)
+    location = models.CharField(max_length=100)
+    event_manager = models.BooleanField(default=True)
+    # Contact = models.PhoneNumberField(null=False, blank=False, unique=True)
+    Contact = models.CharField(max_length=10, default = "12345678")
+    date_posted = models.DateTimeField(default=timezone.now)
+    ngolat = models.FloatField(blank = True, null = True)
+    ngolong = models.FloatField(blank = True, null = True)
+
+    def save(self, *args, **kwargs):
+        g = geocoder.mapbox(self.location, key = access_token)
+        g = g.latlng
+        self.ngolat = g[0]
+        self.ngolong = g[1]
+        return super(Event, self).save(*args, **kwargs)
+
+class Restraunts(models.Model):
+    r_name = models.CharField(max_length=100)
+    r_location = models.CharField(max_length=100)
+    r_contact = models.CharField(max_length=10)
+    average_food = models.IntegerField()
+    approx_people = models.IntegerField()
+    ngolat = models.FloatField(blank = True, null = True)
+    ngolong = models.FloatField(blank = True, null = True)
+
+    def save(self, *args, **kwargs):
+        g = geocoder.mapbox(self.r_location, key = access_token)
+        g = g.latlng
+        self.ngolat = g[0]
+        self.ngolong = g[1]
+        return super(Restraunts, self).save(*args, **kwargs)
---
+++ addresses/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import AddressView
+from . import views
+
+
+urlpatterns = [
+    
+    path('home/', AddressView.as_view(), name='home'),
+    path('register/', views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='addresses/login.html'), name='login'),   
+    path('logout/', views.logout, name='logout'),
+    path('addngo/', views.addngo, name='addngo'),
+    path('addevent/', views.addevent, name='addevent'),
+    path('addrestraunts/', views.addrestraunts, name='addrestraunts'),
+    path('ngohome/', views.ngohome, name='ngohome'),
+    path('eventhome/', views.eventhome, name='eventhome'),
+    path('deleteevent/<ename>/', views.deleteevent, name='deleteevent'),
+]
